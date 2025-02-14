9eb4ebdedb82ed62ed5001d522c8ebb1581c46bd
ranup <2658981151@qq.com>
2025-02-14 17:47:20 +0800

    feat: 404页面

---
+++ pages/[...slug].vue
@@ -0,0 +1,21 @@
+<template>
+  <el-empty :image-size="200" description="暂无此页面" />
+</template>
+
+<script setup>
+definePageMeta({
+  name: 'emptyPage',
+  layout: 'empty'
+})
+</script>
+<style scoped lang="scss">
+.el-empty {
+  width: 100vw;
+  height: 100vh;
+  @include normalFlex(column, center);
+  align-items: center;
+}
+::v-deep(.el-empty__description p) {
+  font-size: 26px !important;
+}
+</style>
