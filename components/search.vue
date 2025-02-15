a5918376e7395649235f75c479e8c9213b817137
ranup <2658981151@qq.com>
2025-02-15 23:16:51 +0800

    feat: 导航栏搜索组件封装

---
+++ components/search.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="search">
+    <input type="text" placeholder="Reno9 系列" />
+    <img class="search-svg" src="@/assets/images/search.svg" alt="" />
+  </div>
+</template>
+
+<style scoped lang="scss">
+.search {
+  @include normalFlex();
+  align-items: center;
+  padding: 8px 16px;
+  width: 160px;
+  height: 36px;
+  background-color: #f6f6f6;
+  box-sizing: border-box;
+  border-radius: 36px;
+  cursor: pointer;
+  input {
+    opacity: 0.3;
+    width: 112px;
+    height: 21px;
+    font-size: 14px;
+    border: none;
+    outline: none;
+    background-color: transparent;
+  }
+  .search-svg {
+    width: 14px;
+    height: 14px;
+  }
+}
+</style>
