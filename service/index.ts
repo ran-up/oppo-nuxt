04875a01aa0f7bb8f8207dd6c280480b194a72dc
ranup <2658981151@qq.com>
2025-02-15 23:16:33 +0800

    feat: useFetch接口封装

---
+++ service/index.ts
@@ -0,0 +1,47 @@
+import type { AsyncData, UseFetchOptions } from '#app'
+
+const BASE_URL = 'http://codercba.com:9060/oppo-nuxt/api'
+export type methodType = 'GET' | 'POST'
+export interface IResponseResult<T> {
+  code: number
+  data: T
+}
+
+class RXRequest {
+  request<T>(
+    url: string,
+    method: methodType,
+    data?: any,
+    options?: UseFetchOptions<T, any, any>
+  ): Promise<AsyncData<T, any>> {
+    return new Promise((resolve, reject) => {
+      const newOptions: UseFetchOptions<T, any, any> = {
+        baseURL: BASE_URL,
+        method: method || 'GET',
+        ...options
+      }
+      if (method === 'GET') {
+        newOptions.query = data
+      } else {
+        newOptions.body = data
+      }
+      useFetch<T>(url, newOptions as any)
+        .then(res => {
+          resolve(res as AsyncData<T, any>)
+        })
+        .catch(err => {
+          reject(err)
+        })
+    })
+  }
+
+  get<T>(url: string, data?: any, options?: UseFetchOptions<T, any, any>) {
+    return this.request<T>(url, 'GET', data, options)
+  }
+
+  post<T>(url: string, params?: any, options?: UseFetchOptions<T, any, any>) {
+    return this.request<T>(url, 'POST', params, options)
+  }
+}
+
+export default new RXRequest()
