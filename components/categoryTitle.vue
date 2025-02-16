e20e11faa05371459ab36e41a3eaf18deaee48bc
ranup <2658981151@qq.com>
2025-02-16 23:18:13 +0800

    feat: 分类信息title完成

---
+++ components/categoryTitle.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="category-title">
+    <h2>{{ title }}</h2>
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps({ title: String })
+</script>
+
+<style scoped lang="scss">
+.category-title {
+  h2 {
+    margin: 0 0 24px 0;
+    padding-top: 60px;
+    font-size: 24px;
+    font-weight: 500;
+  }
+}
+</style>
