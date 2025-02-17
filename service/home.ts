8b42433cf265f61b7ea552d6aaf873a24d6101cd
ranup <2658981151@qq.com>
2025-02-17 17:57:36 +0800

    feat: 首页数据请求

--- service/home.ts
+++ service/home.ts
@@ -2,6 +2,15 @@ import RXRequest from './index'
 import type { IResponseResult } from './index'
 import type { IHomeInfo } from '@/store/home'
 
-export default function getHomeInfoReq() {
-  return RXRequest.get<IResponseResult<IHomeInfo>>('/home/info1', { type: 'oppo' })
+// 首页（OPPO专区）数据请求
+export const getHomeInfoReq = () => {
+  return RXRequest.get<IResponseResult<IHomeInfo>>('/home/info', { type: 'oppo' })
+}
+// OnePlus专区数据请求
+export const getHomeOnePlusInfoReq = () => {
+  return RXRequest.get<IResponseResult<IHomeInfo>>('/home/info', { type: 'onePlus' })
+}
+// 智能硬件数据请求
+export const getHomeIntelligentInfoReq = () => {
+  return RXRequest.get<IResponseResult<IHomeInfo>>('/home/info', { type: 'intelligent' })
 }
