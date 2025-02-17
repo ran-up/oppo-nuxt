82415f69a600dc3e118d96558eb03cc6b9869763
ranup <2658981151@qq.com>
2025-02-17 17:54:38 +0800

    update: 默认布局

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,41 +1,57 @@
 <template>
   <div>
+    <!-- 头部 -->
     <app-header />
+    <!-- 导航 -->
     <navbar :navbars="homeInfoStore.navbars" />
     <slot></slot>
+    <!-- 尾部 -->
     <app-footer />
   </div>
 </template>
 
-<script setup>
-import getHomeInfoReq from '~/service/home'
+<script setup lang="ts">
+import { getHomeInfoReq } from '~/service/home'
 import { useHomeInfoStore } from '~/store/home'
+import type { Categorys, Navbars } from '~/store/home'
 import homeInfoSave from '~/service/saveData/homeInfo'
 
-const homeInfoStore = useHomeInfoStore()
-const { data } = await getHomeInfoReq()
-const errorSolve = () => {
-  // client 和 server 需要分开处理
-  if (import.meta.client) {
-    ElMessage.error('获取首页数据失败，使用备份数据')
-  } else {
-    console.log('获取首页数据失败，使用备份数据')
+// 首页和默认布局中都获取了首页数据。
+// 首页和其余页面的切换都需要重新获取首页数据
+// 不同的是，默认布局只在详情页才进行获取首页数据。
+const homeInfoStore = useHomeInfoStore() // pinia 仓库
+// 在详情页刷新时请求首页数据
+if (homeInfoStore.banners.length === 0) {
+  const { data } = await getHomeInfoReq() // 请求首页数据
+  // 错误处理
+  const errorSolve = () => {
+    // client 和 server 需要分开处理
+    if (import.meta.client) {
+      ElMessage.error('获取首页数据失败，使用备份数据')
+    } else {
+      console.log('获取首页数据失败，使用备份数据')
+    }
+    // 使用备份数据
+    const homeInfo = ref(homeInfoSave.data)
+    // 更新pinia仓库数据
+    homeInfoStore.updateNavbar(homeInfo.value.navbars as Navbars)
+    homeInfoStore.updateBanner(homeInfo.value.banners)
+    homeInfoStore.updateCategory(homeInfo.value.categorys as unknown as Categorys)
   }
-  const homeInfo = ref(homeInfoSave.data)
-  homeInfoStore.updateNavbar(homeInfo.value.navbars)
-  homeInfoStore.updateBanner(homeInfo.value.banners)
-  homeInfoStore.updateCategory(homeInfo.value.categorys)
-}
-if (!data.value) {
-  errorSolve()
-} else {
-  const { code, data: curData } = data.value
-  if (code === 200) {
-    homeInfoStore.updateNavbar(curData.navbars)
-    homeInfoStore.updateBanner(curData.banners)
-    homeInfoStore.updateCategory(curData.categorys)
-  } else {
+  // 接口请求失败则使用备份数据
+  if (!data.value) {
     errorSolve()
+  } else {
+    const { code, data: curData } = data.value
+    if (code === 200) {
+      // 更新pinia仓库数据
+      homeInfoStore.updateNavbar(curData.navbars)
+      homeInfoStore.updateBanner(curData.banners)
+      homeInfoStore.updateCategory(curData.categorys)
+    } else {
+      // 返回数据失败则使用备份数据
+      errorSolve()
+    }
   }
 }
 </script>
