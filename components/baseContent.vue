6c90fb49ea1ec793710785729281b2eaadeae746
ranup <2658981151@qq.com>
2025-02-18 18:07:47 +0800

    feat: 统一管理组件

---
+++ components/baseContent.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <!-- 轮播图 -->
+    <swiper :banners="banners" />
+    <!-- 分类导航 -->
+    <tab-category :categorys="categorys" />
+    <!-- 分类详情 -->
+    <template v-for="category in categorys" :key="category.id">
+      <!-- 单个分类详情 -->
+      <category-section :category="category" />
+    </template>
+  </div>
+</template>
+
+<script setup lang="ts">
+import type { IBanner, ICategory } from '~/store/home'
+
+defineProps({
+  banners: {
+    type: Array<IBanner>,
+    default: () => []
+  },
+  categorys: {
+    type: Array<ICategory>,
+    default: () => []
+  }
+})
+</script>
