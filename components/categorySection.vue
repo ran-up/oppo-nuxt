8ee9678bf32c96d17925dd145be4cdc5a10294ce
ranup <2658981151@qq.com>
2025-02-18 18:09:29 +0800

    feat: 分类

---
+++ components/categorySection.vue
@@ -0,0 +1,18 @@
+<template>
+  <!-- 分类信息存在才展示这个页面，没有不展示 -->
+  <div class="category-section" v-if="(category?.productDetailss ?? []).length > 0">
+    <!-- 分类标题 -->
+    <category-title :title="category?.title" />
+    <!-- 分类item，包含大图介绍和小的item -->
+    <category-grid :categoryUrl="category?.url" :productDetailss="category?.productDetailss" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import type { ICategory } from '~/store/home'
+
+interface prop {
+  category?: ICategory
+}
+const { category } = defineProps<prop>()
+</script>
