b307e4c45e89d0b9ebc1b14262ae988654a96185
ranup <2658981151@qq.com>
2025-02-22 19:36:20 +0800

    update: 将公共内容抽离

--- layouts/default.vue
+++ layouts/default.vue
@@ -13,7 +13,6 @@
 <script setup lang="ts">
 import { getHomeInfoReq } from '~/service/home'
 import { useHomeInfoStore } from '~/store/home'
-import type { Categorys, Navbars } from '~/store/home'
 import homeInfoSave from '~/service/saveData/homeInfo'
 
 // 首页和默认布局中都获取了首页数据。
@@ -23,35 +22,6 @@ const homeInfoStore = useHomeInfoStore() // pinia 仓库
 // 在详情页刷新时请求首页数据
 if (homeInfoStore.banners.length === 0) {
   const { data } = await getHomeInfoReq() // 请求首页数据
-  // 错误处理
-  const errorSolve = () => {
-    // client 和 server 需要分开处理
-    if (import.meta.client) {
-      ElMessage.error('获取首页数据失败，使用备份数据')
-    } else {
-      console.log('获取首页数据失败，使用备份数据')
-    }
-    // 使用备份数据
-    const homeInfo = ref(homeInfoSave.data)
-    // 更新pinia仓库数据
-    homeInfoStore.updateNavbar(homeInfo.value.navbars as Navbars)
-    homeInfoStore.updateBanner(homeInfo.value.banners)
-    homeInfoStore.updateCategory(homeInfo.value.categorys as unknown as Categorys)
-  }
-  // 接口请求失败则使用备份数据
-  if (!data.value) {
-    errorSolve()
-  } else {
-    const { code, data: curData } = data.value
-    if (code === 200) {
-      // 更新pinia仓库数据
-      homeInfoStore.updateNavbar(curData.navbars)
-      homeInfoStore.updateBanner(curData.banners)
-      homeInfoStore.updateCategory(curData.categorys)
-    } else {
-      // 返回数据失败则使用备份数据
-      errorSolve()
-    }
-  }
+  reqUnite(homeInfoStore, data, homeInfoSave) // 统一处理请求数据
 }
 </script>
