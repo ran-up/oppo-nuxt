a3f4ef97f6555faed40384e39562927e68dc94d4
ranup <2658981151@qq.com>
2025-02-22 19:36:30 +0800

    update: 将公共内容抽离

--- pages/intelligent.vue
+++ pages/intelligent.vue
@@ -12,41 +12,11 @@
 <script setup lang="ts">
 import { getHomeIntelligentInfoReq } from '~/service/home'
 import { useHomeInfoStore } from '~/store/home'
-import type { Categorys, Navbars } from '~/store/home'
 import homeIntelligentInfoSave from '~/service/saveData/homeIntelligentInfo'
 
 const homeInfoStore = useHomeInfoStore() // pinia 仓库
 const { data } = await getHomeIntelligentInfoReq() // 请求数据
-// 错误处理
-const errorSolve = () => {
-  // client 和 server 需要分开处理
-  if (import.meta.client) {
-    ElMessage.error('获取智能硬件数据失败，使用备份数据')
-  } else {
-    console.log('获取智能硬件数据失败，使用备份数据')
-  }
-  // 使用备份数据
-  const homeIntelligentInfo = ref(homeIntelligentInfoSave.data)
-  // 更新pinia仓库数据
-  homeInfoStore.updateNavbar(homeIntelligentInfo.value.navbars as Navbars)
-  homeInfoStore.updateBanner(homeIntelligentInfo.value.banners)
-  homeInfoStore.updateCategory(homeIntelligentInfo.value.categorys as unknown as Categorys)
-}
-// 接口请求失败则使用备份数据
-if (!data.value) {
-  errorSolve()
-} else {
-  const { code, data: curData } = data.value
-  if (code === 200) {
-    // 更新pinia仓库数据
-    homeInfoStore.updateNavbar(curData.navbars)
-    homeInfoStore.updateBanner(curData.banners)
-    homeInfoStore.updateCategory(curData.categorys)
-  } else {
-    // 返回数据失败则使用备份数据
-    errorSolve()
-  }
-}
+reqUnite(homeInfoStore, data, homeIntelligentInfoSave) // 统一处理请求数据
 </script>
 
 <style scoped lang="scss">
